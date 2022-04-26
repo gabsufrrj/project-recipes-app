57e73947d64ee0dbe20728248924660dc9814c6c
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-04-26 20:04:21 -0300

    Correct pages location and route order

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Provider from './Context/Provider';
-import FoodRecipes from './Páginas/FoodRecipes';
-import Profile from './Páginas/Profile';
+import FoodRecipes from './pages/FoodRecipes';
+import Profile from './pages/Profile';
 import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,9 +12,9 @@ function App() {
     <Provider>
       <BrowserRouter>
         <Switch>
+          <Route exact path="/"><Login /></Route>
           <Route path="/foods" render={ (props) => <FoodRecipes { ...props } /> } />
           <Route path="/profile" component={ Profile } />
-          <Route exact path="/"><Login /></Route>
         </Switch>
       </BrowserRouter>
     </Provider>
