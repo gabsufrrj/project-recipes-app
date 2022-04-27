25ec3f3a1017f6482e56dd2b51506710db46e3b5
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-04-26 22:54:13 -0300

    Remove prop history from FoodRecipes

--- src/pages/FoodRecipes.jsx
+++ src/pages/FoodRecipes.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Header from '../Components/Header';
 
-function FoodRecipes({ history }) {
+function FoodRecipes() {
   return (
     <div>
-      <Header history={ history } title="Foods" />
+      <Header title="Foods" />
       <h1>
         FoodRecipes
       </h1>
     </div>
   );
 }
-
-FoodRecipes.propTypes = {
-  history: PropTypes.objectOf(PropTypes.any).isRequired,
-};
 
 export default FoodRecipes;
