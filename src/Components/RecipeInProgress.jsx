b4c4ab342ca7322ae1d91442348500c4015db9bb
lucas_almeida <lucas.almdiniz@gmail.com>
2022-05-02 16:19:35 -0300

    Req 47 - criado o component RecipeInProgress que serve para ambos os tipos de receitas (Food e Drink)

---
+++ src/Components/RecipeInProgress.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+
+function RecipeInProgress({ id, apiName }) {
+  const [detailsRecipe, setDatailsRecipe] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const fetchDetailsRecipe = async () => {
+      const request = await fetch(`https://www.${apiName}.com/api/json/v1/1/lookup.php?i=${id}`);
+      const json = await request.json();
+      const datailsRecipeObject = Object.values(json)[0][0];
+      setDatailsRecipe(datailsRecipeObject);
+    };
+    fetchDetailsRecipe();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const typeOfRecipe = () => {
+    const { location } = history;
+    return (location.pathname.includes('foods') ? 'Meal' : 'Drink');
+  };
+
+  const getIngredients = () => {
+    const ingredients = [];
+    const datailsRecipeKeys = Object.keys(detailsRecipe);
+    const ingredientsKeys = datailsRecipeKeys.filter((e) => e.includes('strIngredient'));
+    ingredientsKeys.forEach((e) => (
+      (detailsRecipe[e]) && ingredients.push(detailsRecipe[e])));
+    return ingredients;
+  };
+
+  return (
+    <section>
+      {(detailsRecipe) && (
+        <>
+          <h2 data-testid="recipe-title">{detailsRecipe[`str${typeOfRecipe()}`]}</h2>
+          <h3 data-testid="recipe-category">{detailsRecipe.strCategory}</h3>
+          <img
+            data-testid="recipe-photo"
+            src={ detailsRecipe[`str${typeOfRecipe()}Thumb`] }
+            alt={ detailsRecipe[`str${typeOfRecipe()}`] }
+          />
+          <button data-testid="share-btn" type="button">Compartilhar</button>
+          <button data-testid="favorite-btn" type="button">Favoritar</button>
+          <ul>
+            {getIngredients().map((e, index) => (
+              <li key={ e } data-testid={ `${index}-ingredient-step` }>{e}</li>
+            ))}
+          </ul>
+          <p data-testid="instructions">{detailsRecipe.strInstructions}</p>
+          <button data-testid="finish-recipe-btn" type="button">Finalizar</button>
+        </>)}
+    </section>
+  );
+}
+
+export default RecipeInProgress;
+
+RecipeInProgress.propTypes = {
+  id: PropTypes.string.isRequired,
+  apiName: PropTypes.string.isRequired,
+};
--- src/pages/ProgressDrinkRecipes.jsx
+++ src/pages/ProgressDrinkRecipes.jsx
@@ -1,60 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
+import RecipeInProgress from '../Components/RecipeInProgress';
 
-function ProgressDrinkRecipes(props) {
-  const [detailsRecipe, setDatailsRecipe] = useState(null);
-  const history = useHistory();
-
-  useEffect(() => {
-    const fetchDetailsRecipe = async () => {
-      const { match: { params: { id } } } = props;
-      const request = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-      const json = await request.json();
-      const datailsRecipeObject = Object.values(json)[0][0];
-      setDatailsRecipe(datailsRecipeObject);
-      console.log(datailsRecipeObject);
-    };
-    fetchDetailsRecipe();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const typeOfRecipe = () => {
-    const { location } = history;
-    return (location.pathname.includes('foods') ? 'Meal' : 'Drink');
-  };
-
-  const getIngredients = () => {
-    const ingredients = [];
-    const datailsRecipeKeys = Object.keys(detailsRecipe);
-    const ingredientsKeys = datailsRecipeKeys.filter((e) => e.includes('strIngredient'));
-    ingredientsKeys.forEach((e) => (
-      (detailsRecipe[e]) && ingredients.push(detailsRecipe[e])));
-    return ingredients;
-  };
-
+function ProgressDrinkRecipes({ match: { params: { id } } }) {
   return (
-    <section>
-      {(detailsRecipe) && (
-        <>
-          <h2 data-testid="recipe-title">{detailsRecipe[`str${typeOfRecipe()}`]}</h2>
-          <h3 data-testid="recipe-category">{detailsRecipe.strCategory}</h3>
-          <img
-            data-testid="recipe-photo"
-            src={ detailsRecipe[`str${typeOfRecipe()}Thumb`] }
-            alt={ detailsRecipe[`str${typeOfRecipe()}`] }
-          />
-          <button data-testid="share-btn" type="button">Compartilhar</button>
-          <button data-testid="favorite-btn" type="button">Favoritar</button>
-          <ul>
-            {getIngredients().map((e, index) => (
-              <li key={ e } data-testid={ `${index}-ingredient-step` }>{e}</li>
-            ))}
-          </ul>
-          <p data-testid="instructions">{detailsRecipe.strInstructions}</p>
-          <button data-testid="finish-recipe-btn" type="button">Finalizar</button>
-        </>)}
-    </section>
+    <RecipeInProgress id={ id } apiName="thecocktaildb" />
   );
 }
 
