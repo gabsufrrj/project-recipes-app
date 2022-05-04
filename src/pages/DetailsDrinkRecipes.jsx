3c3881a6c75bbaa4bee43cd2598d4f960228bc5e
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-05-04 15:11:36 -0300

    Req 36: Add recommendations

--- src/pages/DetailsDrinkRecipes.jsx
+++ src/pages/DetailsDrinkRecipes.jsx
@@ -5,20 +5,29 @@ import DrinkDetails from '../Components/DrinkDetails';
 function DetailsDrinkRecipes({ match }) {
   const [drinkDetails, setDrinkDetails] = useState();
   const [loading, setLoading] = useState(true);
+  const [foods, setFoods] = useState();
   useEffect(() => {
-    const getFoodDetails = async () => {
+    const getDrinkDetails = async () => {
       const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${match.params.id}`);
       const result = await response.json();
       console.log(result);
       setDrinkDetails(result.drinks[0]);
       setLoading(false);
     };
-    getFoodDetails();
+    const getFoods = async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const result = await response.json();
+      setFoods(result);
+    };
+    getDrinkDetails();
+    getFoods();
   }, []);
 
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : <DrinkDetails drinkDetails={ drinkDetails } />}
+      {loading
+        ? <h1>Loading...</h1>
+        : <DrinkDetails drinkDetails={ drinkDetails } recommendation={ foods } />}
     </div>
   );
 }
--- src/pages/DetailsFoodRecipes.jsx
+++ src/pages/DetailsFoodRecipes.jsx
@@ -5,6 +5,7 @@ import FoodDetails from '../Components/FoodDetails';
 function DetailsFoodRecipes({ match }) {
   const [foodDetails, setFoodDetails] = useState();
   const [loading, setLoading] = useState(true);
+  const [drinks, setDrinks] = useState();
   useEffect(() => {
     const getFoodDetails = async () => {
       const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${match.params.id}`);
@@ -13,13 +14,19 @@ function DetailsFoodRecipes({ match }) {
       setFoodDetails(result.meals[0]);
       setLoading(false);
     };
+    const getDrinks = async () => {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const result = await response.json();
+      setDrinks(result);
+    };
     getFoodDetails();
+    getDrinks();
   }, []);
 
   return (
     <div>
       {loading ? <h1>Loading...</h1>
-        : <FoodDetails foodDetails={ foodDetails } food={ foodDetails } />}
+        : <FoodDetails foodDetails={ foodDetails } recommendation={ drinks } />}
     </div>
   );
 }
